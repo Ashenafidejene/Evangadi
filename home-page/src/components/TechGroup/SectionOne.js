3a9d0cfbd330231204132e14e7e6855517345431
Ashenafidejene <ashenafidejene75@gmail.com>
2023-10-21 23:36:41 +0300

    .
    .

--- home-page/src/components/TechGroup/SectionOne.js
+++ home-page/src/components/TechGroup/SectionOne.js
@@ -6,7 +6,7 @@ const SectionOne = () => {
     <div className='TechSection'>
       <Sample image='https://www.evangadi.com/themes/humans//assets/images/misc/icons/sharepoint.png'  title="SharePoint" desc="Organizations use SharePoint to create websites. You can use it as a secure place to store, organize, share, and access information from any device." />
       <Sample image='https://www.evangadi.com/themes/humans//assets/images/misc/icons/csharp.png' title="C#" desc="It is a general-purpose language designed for developing apps on the Microsoft platform and requires the .NET framework on Windows to work." />
-      <Sample Image='https://www.evangadi.com/themes/humans//assets/images/misc/icons/pyton.png' title="Python" desc="Python is an object-oriented, high-level programming language with integrated dynamic semantics primarily for web and app development." />
+      <Sample image='https://www.evangadi.com/themes/humans//assets/images/misc/icons/pyton.png' title="Python" desc="Python is an object-oriented, high-level programming language with integrated dynamic semantics primarily for web and app development." />
     </div>
   )
 }
